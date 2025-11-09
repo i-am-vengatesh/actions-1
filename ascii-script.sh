4f326ec61838c10cd26695ab93a1b7ded31aef77
i-am-vengatesh <vengateshbabuvijayakannan@gmail.com>
2025-11-09 22:19:49 +0530

    multijob

--- ascii-script.sh
+++ ascii-script.sh
@@ -2,7 +2,3 @@
  sudo apt-get install cowsay -y
 #Generate ASCII Artwork
 cowsay -f dragon "Run for cover, I am a DRAGON....RAWR" >> dragon.txt
-#name: Test File Exists
-grep -i "dragon" dragon.txt
-#name: Read File
-cat dragon.txt
